3cdba5f2f9d8339309ae253591be8801d2a1e2e1
caubebuon03 <70497356+caubebuon03@users.noreply.github.com>
2020-10-12 19:02:28 +0700

    update

--- src/app/product/list-brand/list-brand.component.ts
+++ src/app/product/list-brand/list-brand.component.ts
@@ -2,34 +2,35 @@ import { Component, Injector, OnInit } from '@angular/core';
 import { BaseComponent } from 'src/app/lib/base-component';
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  selector: 'app-list-brand',
+  templateUrl: './list-brand.component.html',
+  styleUrls: ['./list-brand.component.css']
 })
-export class ListComponent extends BaseComponent implements OnInit {
-  list: any;
+export class ListBrandComponent extends BaseComponent implements OnInit {
+  list_brand: any;
   page: any;
   pageSize: any;
   totalItems:any;
-  category_id:any;
+  brand_id:any;
   menus:any;
   brands:any;
 
   constructor(injector: Injector) {
     super(injector);
+
    }
 
   ngOnInit(): void {
-    this.list = [];
+    this.list_brand = [];
     this.page = 1;
     this.pageSize = 5;
     this._route.params.subscribe(params => {
-      this.category_id = params['id'];
-      this._api.post('/api/product/search', { 
+      this.brand_id = params['id'];
+      this._api.post('/api/product/search1', { 
         page: this.page, 
         pageSize: this.pageSize, 
-        category_id: this.category_id}).takeUntil(this.unsubscribe).subscribe(res => {
-        this.list = res.data;
+        brand_id: this.brand_id}).takeUntil(this.unsubscribe).subscribe(res => {
+        this.list_brand= res.data;
         this.totalItems = res.totalItems;
         }, err => { });   
         
@@ -44,19 +45,19 @@ export class ListComponent extends BaseComponent implements OnInit {
         }); 
    });   
   }
-
   loadPage(page) { 
     this._route.params.subscribe(params => {
       let id = params['id'];
-      this._api.post('/api/product/search', { 
+      this._api.post('/api/product/search1', { 
         page: page, 
         pageSize: this.pageSize, 
-        category_id: id}).takeUntil(this.unsubscribe).subscribe(res => {
-        this.list = res.data;
+        brand_id: id}).takeUntil(this.unsubscribe).subscribe(res => {
+        this.list_brand = res.data;
         this.totalItems = res.totalItems;
         }, err => { });       
    });   
   }
+
   addToCart(it) { 
     this._cart.addToCart(it);
     alert('Thêm thành công!'); 
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChitietComponent } from '../product/chitiet/chitiet.component';
 import { ListComponent } from './list/list.component';
+import { ListBrandComponent } from './list-brand/list-brand.component';
 
 
 const routes: Routes = [
@@ -15,11 +16,16 @@ const routes: Routes = [
     path: 'list/:id',
     component: ListComponent,
   },
+
+  {
+    path: 'listb/:id',
+    component: ListBrandComponent,
+  },
 ];  
 
 
 @NgModule({
-  declarations: [ChitietComponent, ListComponent],
+  declarations: [ChitietComponent, ListComponent, ListBrandComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
